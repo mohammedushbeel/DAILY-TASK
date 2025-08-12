3e9e07a61428de8b7a600648d89bd6abe3574180
MUHAMMED USHBEEL <ushbeel1921@gmail.com>
2025-08-12 12:20:10 +0530

    message

---
+++ task6.js
@@ -0,0 +1,19 @@
+
+
+ 
+ function a(b){
+
+     
+     if(b%3===0 && b%5===0){
+        console.log("fizzbuzz");
+        }else if(b%3===0){
+            console.log("buzz");    
+        }else{
+            console.log("fizz");
+            
+        }
+     
+        }
+        a(3)
+ 
+
